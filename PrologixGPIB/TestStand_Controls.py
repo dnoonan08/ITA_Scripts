65fb87aab499b384d880e53bb17be3690077ed57
Danny Noonan <dnoonan@cern.ch>
2022-06-22 12:25:52 -0500

    update script for prologix gpib control

--- PrologixGPIB/TestStand_Controls.py
+++ PrologixGPIB/TestStand_Controls.py
@@ -3,6 +3,7 @@ from time import sleep
 
 GPIBAddresses={46:6,
               48:8,
+              99:5,
               }
 
 class psControl:
@@ -18,50 +19,62 @@ class psControl:
         self.gpib.disconnect()
 
     def select(self,board):
-        self.gpib.select(GPIBAddresses[board])
-        self.board=board
-
-    def ASICOn(self,board=None):
         if not board is None:
             self.gpib.select(GPIBAddresses[board])
             self.board=board
-        else:
-            print(f'Turning on ASIC for board {board}')
 
-        self.gpib.write("VOLT 1.2")
-        self.gpib.write("CURR 0.6")
-        self.gpib.write("OUTP ON")
+    def ID(self,board):
+        self.select(board)
+        return self.gpib.query("*IDN?")[:-1]
+
+    def SetVoltage(self, board, voltage):
+        self.select(board)
+
+        if voltage<=1.5 and voltage >= 0.9:
+            self.gpib.write(f"VOLT {voltage}")
+            return True
+        else:
+            print(f'Selected voltage ({voltage}) outside of defined safe range 0.9-1.5')
+            return False
+
+    def ASICOn(self,board=None,voltage=None):
+        self.select(board)
+        x=self.gpib.query('++addr')
+
+        if voltage is None:
+            is_set=self.SetVoltage(None,1.2)
+        else:
+            is_set=self.SetVoltage(None,float(voltage))
+
+        if is_set:
+            self.gpib.write("CURR 0.6")
+            self.gpib.write("OUTP ON")
 
     def ASICOff(self,board=None):
-        if not board is None:
-            self.gpib.select(GPIBAddresses[board])
-            self.board=board
-        else:
-            print(f'Turning off ASIC for board {board}')
-
-        self.gpib.select(GPIBAddresses[board])
+        self.select(board)
+        x=self.gpib.query('++addr')
         self.gpib.write("OUTP OFF")
 
     def Read_Power(self,board=None):
-        if not board is None:
-            self.gpib.select(GPIBAddresses[board])
-            self.board=board
-        else:
-            print(f'Reading from board {board}')
+        self.select(board)
 
-        v=self.gpib.query("MEAS:VOLT?")[:-2]
-        i=self.gpib.query("MEAS:CURR?")[:-2]
-        return v,i
+        output=self.gpib.query("OUTP?")[:-1]
+        v=self.gpib.query("MEAS:VOLT?")[:-1]
+        i=self.gpib.query("MEAS:CURR?")[:-1]
+        return output,v,i
+        
 
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--On', default=False, action='store_true', help='Turn on ECON-T ASIC')
-    parser.add_argument('--Off', default=False, action='store_true', help='Turn off ECON-T ASIC')
+    parser.add_argument('--On', '--on', default=False, action='store_true', help='Turn on ECON-T ASIC')
+    parser.add_argument('--Off', '--off', default=False, action='store_true', help='Turn off ECON-T ASIC')
     parser.add_argument('--disconnect', default=False, action='store_true', help='Disconnect gpib (set back to local mode')
     parser.add_argument('--read', default=False, action='store_true', help='Read power')
+    parser.add_argument('--id', default=False, action='store_true', help='Get ID')
     parser.add_argument('--logging', default=False, action='store_true', help='Start power monitoring')
+    parser.add_argument('--setVoltage', default=None, help='Voltage setting (1.2 V if left unset)')
     parser.add_argument('--logName', default='logFile.log', help='log name')
     parser.add_argument('--time', default=15, type=float,help='Frequency (in seconds) of how often to read the power')
     parser.add_argument('--ip', default='192.168.0.50', help='IP Address of the gpib controller')
@@ -71,13 +84,18 @@ if __name__=='__main__':
 
     ps=psControl(host=args.ip)
 
+
     if args.On:
-        ps.ASICOn(args.board)
+        ps.ASICOn(args.board,args.setVoltage)
     if args.Off:
         ps.ASICOff(args.board)
+    if not args.On and not args.setVoltage is None:
+        ps.SetVoltage(args.board,args.voltage)
+    if args.id:
+        print(ps.ID(args.board))
     if args.read:
-        v,i=ps.Read_Power(args.board)
-        print(v,i)
+        p,v,i=ps.Read_Power(args.board)
+        print(f'Power: {"On" if int(p) else "Off"}, Voltage: {float(v):.4f} V, Current: {float(i):.4f} A')
     if args.logging:
         import logging
         import time
@@ -93,8 +111,8 @@ if __name__=='__main__':
 
         try:
             while True:
-                v_ASIC,i_ASIC=ps.Read_Power(args.board)
-                logging.info(f'ASIC Voltage: {v_ASIC}, ASIC Current:{i_ASIC}')
+                on_ASIC,v_ASIC,i_ASIC=ps.Read_Power(args.board)
+                logging.info(f'Power: {"On" if int(p) else "Off"}, ASIC Voltage: {float(v_ASIC):.4f}, ASIC Current:{float(i_ASIC):.4f}')
                 sleep(args.time)
         except KeyboardInterrupt:
             logging.info(f'Closing')
